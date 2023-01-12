5a21e5f69f453c09cd86610e0241a60add48a026
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-01-12 12:37:31 +0100

    Working adding book function

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const MODAL = document.querySelector('.new-book__modal');
 const LIBRARY_CONTAINER = document.getElementById('library-container');
 
 // basic functions to add books to myLibrary array and display library
-let Book = function (title, author, read) {
+let Book = function (title, author, status) {
   this.title = title,
   this.author = author,
-  this.read = read,
+  this.status = status,
   this.info = function () {
-    return `${title} is a book by ${author}. I have ${read} it.`;
+    return `${title} is a book by ${author}. I have ${status} it.`;
   };
 };
 
@@ -26,19 +26,19 @@ for (const Book of myLibrary) {
 };
 
 // Open add book form
-ADD_BOOK_BUTTON.addEventListener('click', () => {
-  BACKDROP.style.display = 'block';
-  MODAL.style.display = 'block';
-});
-
-BACKDROP.addEventListener('click', () => {
+const closeModal = () => {
   BACKDROP.style.display = 'none';
   MODAL.style.display = 'none';
-});
+};
 
-// Get book data :
-// Get input from the form,
-// Assign inputs to new Book constructor, and add the book to the library
+const openModal = () => {
+  BACKDROP.style.display = 'block';
+  MODAL.style.display = 'block';
+};
+
+ADD_BOOK_BUTTON.addEventListener('click', () => openModal());
+BACKDROP.addEventListener('click', () => closeModal());
+
 // Create a book card for each item in array
 const updateLibrary = function () {
   for (const Book of myLibrary) {
@@ -55,14 +55,61 @@ const updateLibrary = function () {
     BOOK_CARD.appendChild(BOOK_CARD_AUTHOR);
     const BOOK_CARD_STATUS = document.createElement('div'); // add book status div
     BOOK_CARD_STATUS.classList.add('book-card__status');
-    if (Book.read === 'read') {
+    if (Book.status === 'read') {
       BOOK_CARD_STATUS.classList.add('read');
       } else {
       BOOK_CARD_STATUS.classList.add('not-read');
     }
-    BOOK_CARD_STATUS.textContent = Book.read;
+    BOOK_CARD_STATUS.textContent = Book.status;
     BOOK_CARD.appendChild(BOOK_CARD_STATUS);
   }
+  toggleBookStatus();
+};
+
+// Get book data from the form,
+function getBookData (form) {
+  let bookTitle = form.title.value;
+  let bookAuthor = form.author.value;
+  let bookStatus = form.read.value;
+  let addedBook = new Book (bookTitle, bookAuthor, bookStatus);
+  myLibrary.push(addedBook);
+  form.title.value = '';
+  form.author.value = '';
+  closeModal();
+  const BOOK_CARD = document.createElement('div'); // create book card
+  BOOK_CARD.classList.add('book-card');
+  LIBRARY_CONTAINER.appendChild(BOOK_CARD);
+  const BOOK_CARD_TITLE = document.createElement('div'); // add book title div
+  BOOK_CARD_TITLE.classList.add('book-card__title');
+  BOOK_CARD_TITLE.textContent = Book.title;
+  BOOK_CARD.appendChild(BOOK_CARD_TITLE);
+  const BOOK_CARD_AUTHOR = document.createElement('div'); // add book title div
+  BOOK_CARD_AUTHOR.classList.add('book-card__author');
+  BOOK_CARD_AUTHOR.textContent = Book.author;
+  BOOK_CARD.appendChild(BOOK_CARD_AUTHOR);
+  const BOOK_CARD_STATUS = document.createElement('div'); // add book status div
+  BOOK_CARD_STATUS.classList.add('book-card__status');
+  if (Book.status === 'read') {
+    BOOK_CARD_STATUS.classList.add('read');
+    } else {
+    BOOK_CARD_STATUS.classList.add('not-read');
+  }
+  BOOK_CARD_STATUS.textContent = Book.status;
+  BOOK_CARD.appendChild(BOOK_CARD_STATUS);
+}
+
+let toggleBookStatus = function () {
+  let bookStatusButton = document.querySelectorAll('.book-card__status'); // toggle read status on click
+  for (let i = 0; i < bookStatusButton.length; i++) {
+    bookStatusButton[i].addEventListener('click', function () {
+      this.classList.toggle('read');
+      if (this.textContent === 'read') {
+        this.textContent = 'not read';
+      } else {
+        this.textContent = 'read';
+      }
+    });
+  }  
 };
 
 //Quick test setup
